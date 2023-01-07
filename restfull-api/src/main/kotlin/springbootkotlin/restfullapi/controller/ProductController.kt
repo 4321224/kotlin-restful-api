1527e487c3c115be7630b12c9b5f2346d3083ff6
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-07 07:49:49 +0700

    create product restfull api

--- restfull-api/src/main/kotlin/springbootkotlin/restfullapi/controller/ProductController.kt
+++ restfull-api/src/main/kotlin/springbootkotlin/restfullapi/controller/ProductController.kt
@@ -1,8 +1,25 @@
 package springbootkotlin.restfullapi.controller
 
 import org.springframework.web.bind.annotation.*
+import springbootkotlin.restfullapi.model.CreateProduct
+import springbootkotlin.restfullapi.model.ProductResponse
+import springbootkotlin.restfullapi.model.WebResponse
+import springbootkotlin.restfullapi.service.ProductService
 
 
 @RestController
-class ProductController {
+class ProductController (val productService: ProductService){
+    @PostMapping(
+        value = ["/api/products"],
+        produces = ["application/json"],
+        consumes = ["application/json"]
+    )
+    fun createProducts(@RequestBody body: CreateProduct): WebResponse<ProductResponse> {
+        val productResponse = productService.create(body)
+        return WebResponse(
+            code = 200,
+            status = "OK",
+            data = productResponse
+        )
+    }
 }
